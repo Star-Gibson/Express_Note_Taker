050d2e0a7f7823294f6b19f83373d297dc652bab
Star-Gibson <sgibsoncoding@gmail.com>
2020-12-02 21:16:17 -0500

    Added Listener, Added basic route to index.html

--- server.js
+++ server.js
@@ -13,5 +13,19 @@ var PORT = 3000;
 //Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+//Sets up Express to serve static files
+app.use(express.static("public"));
+
+//Routes
+//Basic route - sends user to index page
+app.get("/", function(req, res){
+    res.sendFile(path.join(_dirname, "public/index.html"));
+});
+//Basic route - sends user to notes page
 
 
+
+//Listener
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+});
